145b4ef6363925e719a9dbeec421f40f482c0595
Audi <audiwu321@gmail.com>
2023-05-04 19:25:36 +0800

    feat: replace all

---
+++ src/medium/replace-all/index.ts
@@ -0,0 +1,10 @@
+export type ReplaceAll<
+  S extends string,
+  From extends string,
+  To extends string,
+  Before extends string = ""
+> = From extends ""
+  ? S
+  : S extends `${Before}${infer L}${From}${infer R}`
+  ? ReplaceAll<`${Before}${L}${To}${R}`, From, To, `${Before}${L}${To}`>
+  : S;
